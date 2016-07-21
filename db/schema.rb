b8470bae0c20e4fdb7d9fd7f56b8f036ac465564
albertbahia <albert.bahia1@gmail.com>
2016-07-21 00:32:25 -0400

    removed date field from cohorts

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160721035216) do
+ActiveRecord::Schema.define(version: 20160721042555) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "cohorts", force: :cascade do |t|
-    t.date     "date"
     t.string   "class_code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
