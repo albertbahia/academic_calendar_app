e6bf56b2585100dbebcfde4807ab2850ef23f280
albertbahia <albert.bahia1@gmail.com>
2016-07-21 14:41:59 -0400

    removed comments

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -28,8 +28,7 @@ class EventsController < ApplicationController
     @event = Event.new(event_params)
     cohort_id = params[:event][:cohort]
     @event.cohort_id = cohort_id
-    # @event = @cohort.events.create(created_at: Time.now)
-    binding.pry
+
     respond_to do |format|
       if @event.save
         format.html { redirect_to @event, notice: 'Event was successfully created.' }
