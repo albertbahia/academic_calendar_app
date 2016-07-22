fcaadba8e0be05c78ec1160d867487c52624471a
albertbahia <albert.bahia1@gmail.com>
2016-07-21 20:29:48 -0400

    removed unncessary code

--- app/models/cohort.rb
+++ app/models/cohort.rb
@@ -1,19 +1,3 @@
 class Cohort < ApplicationRecord
   has_many :events, dependent: :destroy
-
-  def self.find_matched_events
-    # events = Event.where(cohort_id: self.ids)
-    # matched_users.each do |matched_user|
-		# 	Match.find_or_create_by({
-		# 		user_id1: self.id,
-		# 		user_id2: matched_user.id
-		# 		})
-		# end
-
-    cohorts = self.ids.map do |id|
-      Event.where(cohort_id: id)
-      # binding.pry
-    end
-    # binding.pry
-  end
 end
